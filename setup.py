ad9dfcfa3dc96f0760937d5cc2c3917eb59ae7d0
unknown <muralim1585@gmail.com>
2023-03-21 21:34:46 -0700

    new release v1.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Murali',                   # Type in your name
   author_email = 'muraliseattle85@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Muralim1585/BigO_TimeComplexity',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Muralim1585/BigO_TimeComplexity/archive/refs/tags/v_1.0.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Muralim1585/BigO_TimeComplexity/archive/refs/tags/v_1.0.1.tar.gz',    # I explain this later on
   keywords = ['BigO', 'Time Complexity', 'Optimization'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
